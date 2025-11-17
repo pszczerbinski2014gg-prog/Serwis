56c81c2b52dd02e28044fa38d8787086e99a2e7b
Patryk <p.szczerbinski2014gg@gmail.com>
2025-11-17 14:14:05 +0100

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('news', { title: 'News' });
+router.get('/', async function (req, res) {
+  try {
+    const search = req.query.search || '';
+    const newsList = await News.find({
+      title: new RegExp(search.trim(), 'i'),
+    }).sort({
+      created: -1,
+    });
+    res.render('news', { title: 'News', data: newsList, search });
+  } catch (err) {
+    console.log('❌ Błąd pobierania newsów:', err);
+    res.render('news', { title: 'News', data: [] });
+  }
 });
 
 module.exports = router;
